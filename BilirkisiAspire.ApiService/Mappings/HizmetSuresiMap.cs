8d9a2eff3b3514cba925489ad1825987ee2c64c2
Tuna Zorlu <yasartunazorlu@gmail.com>
2024-07-01 22:32:12 +0300

    HizmetSüresüMap added

---
+++ BilirkisiAspire.ApiService/Mappings/HizmetSuresiMap.cs
@@ -0,0 +1,16 @@
+﻿using BilirkisiAspire.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BilirkisiAspire.ApiService.Mappings
+{
+    public class HizmetSuresiMap : IEntityTypeConfiguration<HizmetSuresi>
+    {
+        public void Configure(EntityTypeBuilder<HizmetSuresi> builder)
+        {
+            builder.HasKey(t => t.HizmetSuresiId);
+            builder.Property(t => t.HizmetSuresiId).ValueGeneratedOnAdd();
+            builder.HasOne(t => t.MahkemeDosyasi).WithMany(t=> t.HizmetSureleri).HasForeignKey(t=>t.MahkemeDosyasiId);
+        }
+    }
+}
