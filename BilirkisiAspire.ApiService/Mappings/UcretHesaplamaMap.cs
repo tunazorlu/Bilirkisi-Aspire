a3e59846e4d8b27f690b958afba198d680121177
Tuna Zorlu <yasartunazorlu@gmail.com>
2024-07-06 10:42:07 +0300

    UcretHesaplama Mapping done

---
+++ BilirkisiAspire.ApiService/Mappings/UcretHesaplamaMap.cs
@@ -0,0 +1,23 @@
+﻿using BilirkisiAspire.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BilirkisiAspire.ApiService.Mappings
+{
+    public class UcretHesaplamaMap : IEntityTypeConfiguration<UcretHesaplama>
+    {
+        public void Configure(EntityTypeBuilder<UcretHesaplama> builder)
+        {
+            builder.HasKey(t => t.UcretHesaplamaId);
+            builder.Property(t => t.UcretHesaplamaId).ValueGeneratedOnAdd();
+            builder.Property(t => t.TemelUcret).HasMaxLength(9);
+            builder.Property(t => t.ElleGirilenYemekUcreti).HasMaxLength(9);
+            builder.Property(t => t.ElleGirilenYolUcreti).HasMaxLength(9);
+            builder.Property(t => t.YemekUcreti).HasMaxLength(9);
+            builder.Property(t => t.YolUcreti).HasMaxLength(9);
+            builder.Property(t => t.Prim).HasMaxLength(9);
+            builder.Property(t => t.GiydirilmisUcret).HasMaxLength(9);
+            builder.HasOne(t => t.MahkemeDosyasi).WithOne(t => t.UcretHesaplama).HasForeignKey<MahkemeDosyasi>(t => t.UcretHesaplamaId);
+        }
+    }
+}
--- Shared/Models/UcretHesaplama.cs
+++ Shared/Models/UcretHesaplama.cs
@@ -11,28 +11,24 @@ namespace BilirkisiAspire.Shared.Models
         public string? MeslekKodu { get; set; }
         [Display(Name = "Aylık Ortalama Ücret")]
         public string? AylikOrtalamaUcret { get; set; }
-        [Column(TypeName = "decimal(8, 2)")]
+
         [Display(Name = "Temel Ücret")]
         public decimal? TemelUcret { get; set; }
 
-        [Column(TypeName = "decimal(8, 2)")]
         [Display(Name = "Yemek Ücreti")]
         public decimal? YemekUcreti { get; set; }
-        [Column(TypeName = "decimal(8, 2)")]
+
         [Display(Name = " Elle Girilen Yemek Ücreti")]
         public decimal? ElleGirilenYemekUcreti { get; set; }
 
-        [Column(TypeName = "decimal(8, 2)")]
+
         [Display(Name = "Elle Girilen Yol Ücreti")]
         public decimal? ElleGirilenYolUcreti { get; set; }
 
-        [Column(TypeName = "decimal(8, 2)")]
         [Display(Name = "Yol Ücreti")]
         public decimal? YolUcreti { get; set; }
-        [Column(TypeName = "decimal(8, 2)")]
         [Display(Name = "Prim")]
         public decimal? Prim { get; set; }
-        [Column(TypeName = "decimal(8, 2)")]
         [Display(Name = "Giydirilmiş Ücret")]
         public decimal? GiydirilmisUcret { get; set; }
         [Display(Name = "Asgari Ücret Katsayı Al")]
